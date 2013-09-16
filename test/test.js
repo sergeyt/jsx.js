4d43de5b4ff323140c1a21a0bb1b756af1927fe8
sergeyt <stodyshev@gmail.com>
2013-09-16 13:08:54 +0700

    more tests

--- test/test.js
+++ test/test.js
@@ -1,34 +1,86 @@
 describe("jsx.js tests", function() {
 	
 	// Object extensions
+
+	function foo() { return "foo"; }
 	
 	it("Object.isObject", function() {
-		expect(Object.isObject(null)).toBe(false);
-		expect(Object.isObject("test")).toBe(false);
-		expect(Object.isObject(true)).toBe(false);
-		expect(Object.isObject(1)).toBe(false);
-		expect(Object.isObject({})).toBe(true);
-		expect(Object.isObject(new Date())).toBe(true);
-		expect(Object.isObject(function() { return "test"; })).toBe(true);
+		var negative = [null, "test", true, 1];
+		negative.forEach(function(v) {
+			expect(Object.isObject(v)).toBe(false);
+		});
+		var positive = [{}, new Date(), foo];
+		positive.forEach(function(v) {
+			expect(Object.isObject(v)).toBe(true);
+		});
 	});
 	
 	it("Object.isFunction", function() {
-		expect(Object.isFunction(null)).toBe(false);
-		expect(Object.isFunction("test")).toBe(false);
-		expect(Object.isFunction(true)).toBe(false);
-		expect(Object.isFunction(1)).toBe(false);
-		expect(Object.isFunction({})).toBe(false);
-		expect(Object.isFunction(new Date())).toBe(false);
-		expect(Object.isFunction(function() { return "test"; })).toBe(true);
+		var negative = [null, "test", true, 1, {}, new Date()];
+		negative.forEach(function(v) {
+			expect(Object.isFunction(v)).toBe(false);
+		});
+		expect(Object.isFunction(foo)).toBe(true);
 	});
 
 	it("Object.isString", function() {
-		expect(Object.isString(null)).toBe(false);
+		var negative = [null, true, 1, {}, new Date(), foo];
+		negative.forEach(function(v) {
+			expect(Object.isString(v)).toBe(false);
+		});
 		expect(Object.isString("test")).toBe(true);
-		expect(Object.isString(true)).toBe(false);
-		expect(Object.isString(1)).toBe(false);
-		expect(Object.isString({})).toBe(false);
-		expect(Object.isString(new Date())).toBe(false);
-		expect(Object.isString(function() { return "test"; })).toBe(false);
+	});
+
+	it("Object.isNumber", function() {
+		var negative = [null, true, "test", {}, new Date(), foo];
+		negative.forEach(function(v) {
+			expect(Object.isNumber(v)).toBe(false);
+		});
+		expect(Object.isNumber(1)).toBe(true);
+		expect(Object.isNumber(3.14)).toBe(true);
+	});
+
+	// Boolean extensions
+
+	it("Boolean.toBoolean", function() {
+		var t = [true, 1, "test", {}, new Date(), foo];
+		t.forEach(function(v) {
+			expect(Boolean.toBoolean(v)).toBe(true);
+		});
+		var f = [false, 0, null, ""];
+		f.forEach(function(v) {
+			expect(Boolean.toBoolean(v)).toBe(false);
+		});
+	});
+
+	// Number extensions
+
+	it("Number.isNumber", function() {
+		var negative = [null, true, "test", {}, new Date(), foo];
+		negative.forEach(function(v) {
+			expect(Number.isNumber(v)).toBe(false);
+		});
+		expect(Number.isNumber(1)).toBe(true);
+		expect(Number.isNumber(3.14)).toBe(true);
+	});
+
+	// String extensions
+
+	it("String.isString", function() {
+		var negative = [null, true, 1, {}, new Date(), foo];
+		negative.forEach(function(v) {
+			expect(String.isString(v)).toBe(false);
+		});
+		expect(String.isString("test")).toBe(true);
+	});
+
+	// Function extensions
+
+	it("Function.isFunction", function() {
+		var negative = [null, "test", true, 1, {}, new Date()];
+		negative.forEach(function(v) {
+			expect(Function.isFunction(v)).toBe(false);
+		});
+		expect(Function.isFunction(foo)).toBe(true);
 	});
 });
